351aca79f269f20455d52feb17e520adf9ca23a9
Harshvar279 <dgwdzfcg5h@privaterelay.appleid.com>
2026-05-25 17:22:45 +0530

    Create smoothScroll.js

---
+++ Frontend/src/lib/smoothScroll.js
@@ -0,0 +1,15 @@
+import Lenis from '@studio-freight/lenis'
+
+export function initSmoothScroll() {
+  const lenis = new Lenis({
+    duration: 1.4,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    smoothWheel: true,
+  })
+  function raf(time) {
+    lenis.raf(time)
+    requestAnimationFrame(raf)
+  }
+  requestAnimationFrame(raf)
+  return lenis
+}
